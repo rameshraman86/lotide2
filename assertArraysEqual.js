d7157df0ab35f268489fe7c8bce01b76a47ebf5b
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-06-27 15:33:59 -0400

    added logic to console log the result

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`Both the provided arrays are not equal.`);
+      return false;
+    }
+  }
+  console.log(`Both the provided arrays are equal.`)
+  return true;
+};
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
