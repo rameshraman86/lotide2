8b983a3435e678080d43937197c449a5078ab2a4
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-06-27 23:14:00 -0400

    copied assertArraysEqual and eqArrays functions into flatten.js

--- flatten.js
+++ flatten.js
@@ -1,3 +1,41 @@
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    console.log(`Both the provided arrays are not equal.`);
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`Both the provided arrays are not equal.`);
+      return false;
+    }
+  }
+  console.log(`Both the provided arrays are equal.`)
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀Assertion Passed`);
+  } else {
+    console.log(`🥵Assertion Failed`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
 const flatten = function(arr) {
   let flattendedArray = [];
   for (let mainArrVal of arr) {
