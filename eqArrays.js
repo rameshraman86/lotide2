e5de1a08b25891e191d684fe9f71a8632550509f
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-06-28 08:38:05 -0400

    updated eqarrays emoji

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`😀Assertion Passed`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🥵Assertion Failed`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
