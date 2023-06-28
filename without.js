8d9bb160ddf4b1af5345f52df9ade02195e71b8f
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-06-28 08:25:25 -0400

    updated assertArraysEqual

--- without.js
+++ without.js
@@ -1,17 +1,30 @@
-const assertArraysEqual = function(arr1, arr2) {
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀Assertion Passed`);
+  } else {
+    console.log(`🥵Assertion Failed`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    console.log(`Both the provided arrays are not equal.`);
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(`Both the provided arrays are not equal.`);
-      console.log("running this");
       return false;
     }
   }
-  console.log(`Both the provided arrays are equal.`);
   return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`Both the provided arrays are equal.`);
+    return true;
+  }
+  console.log(`Both the provided arrays are not equal.`);
+  return false;
 };
 
 const without = function(source, itemsToRemove) {
