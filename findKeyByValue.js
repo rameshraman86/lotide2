9edbd82fc41819997f46736db1e84315d0869c35
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-06-28 19:59:51 -0400

    full solution

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(obj, value) {
+  for(let key in obj) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), '');
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
