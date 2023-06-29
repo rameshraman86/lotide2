87b58ed3e86e225dcd0541953bc5273098045e17
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-06-29 17:29:30 -0400

    completed assignment

---
+++ findKey.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = function(obj, callback) {
+  for(let key in obj) {
+    if(callback(obj[key])) {
+      return key;
+    }
+  }
+}
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma') // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": 1 ,
+  "Akaleri":  3 ,
+  "noma":     2 ,
+  "elBulli":  3 ,
+  "Ora":      2 ,
+  "Akelarre": 3 
+}, x => x === 2), 'noma') // => "noma"
