ddd09623e5247e8a97af7c4bc2ac27e2c4911305
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-04 07:05:57 -0400

    updated with flatten

--- index.js
+++ index.js
@@ -2,9 +2,20 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const flattenRecursive = require('./flatten');
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const eqObjects = require('./eqObjects');
+
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertArraysEqual,
+  assertEqual,
+  eqObjects,
+  flattenRecursive
 };
