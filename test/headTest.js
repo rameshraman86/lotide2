03fb989730e0063011257003fc6ba8fa7b8203f3
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-04 06:51:45 -0400

    refactor using index

--- test/headTest.js
+++ test/headTest.js
@@ -1,17 +1,14 @@
 const assert = require('chai').assert;
-// const assertEqual = require(`../assertEqual`);
-const head = require('../head');
-
-// assertEqual(head([1,2,3,4,5]), 1); //=> Should pass
-// assertEqual(head([1,"ramesh"]), "ramesh"); //=> Should Fail
+// const head = require('../head');
+const _ = require('../index')
 
 describe('#head', () => {
   it('returns 1 for [1,2,3]', () => {
-    assert.strictEqual(head([1,2,3]), 1);
+    assert.strictEqual(_.head([1,2,3]), 1);
   });
 
   it('returns "5" for [5]', () => {
-    assert.strictEqual(head(['5']), '56');
+    assert.strictEqual(_.head(['5']), '5');
   });
 });
 
