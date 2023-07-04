65a3b25a0291a780ac6879e3bf022cb551c99d26
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-04 06:52:52 -0400

    refactor using index

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,19 +1,19 @@
-const middle = require('../middle');
+const _ = require('../index');
 const assert = require('chai').assert;
 
 
 describe('#middle', () => {
   it('return [] for [1] or [1,2]', () => {
-    assert.deepEqual(middle([1]), []);
-    assert.deepEqual(middle([1,2]), []);
+    assert.deepEqual(_.middle([1]), []);
+    assert.deepEqual(_.middle([1,2]), []);
   });
 
   it('return middle 2 elements for [1, 2, 3, 4]', () => {
-    assert.deepEqual(middle([1, 2, 3, 4]), [2,3]);
+    assert.deepEqual(_.middle([1, 2, 3, 4]), [2,3]);
   });
 
   it('return [3] for [1, 2, 3, 4, 5]', () => {
-    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+    assert.deepEqual(_.middle([1, 2, 3, 4, 5]), [3]);
   })
 });
 
