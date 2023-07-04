2a6a802c22b7743af3af70e903862e51a8f80742
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-04 07:05:01 -0400

    refactored and created test scripts using npm

--- flatten.js
+++ flatten.js
@@ -1,37 +1,16 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Pass. Both arrays are equal.`);
-    return true;
-  }
-  console.log(`🛑🛑🛑 Failed: Both arrays are not equal.`);
-  return false;
-};
-
-const flatten = function(arr) {
-  let flattendedArray = [];
-  for (let mainArrVal of arr) {
-    if (!Array.isArray(mainArrVal)) {
-      flattendedArray.push(mainArrVal);
-    } else {
-      for (let innerArray of mainArrVal) {
-        flattendedArray.push(innerArray);
-      }
-    }
-  }
-  return flattendedArray;
-};
+// const flatten = function(arr) {
+//   let flattendedArray = [];
+//   for (let mainArrVal of arr) {
+//     if (!Array.isArray(mainArrVal)) {
+//       flattendedArray.push(mainArrVal);
+//     } else {
+//       for (let innerArray of mainArrVal) {
+//         flattendedArray.push(innerArray);
+//       }
+//     }
+//   }
+//   return flattendedArray;
+// };
 
 //Recursive method
 const flattenRecursive = function(arr, flattendedArray = []) {
@@ -45,4 +24,4 @@ const flattenRecursive = function(arr, flattendedArray = []) {
   return flattendedArray;
 };
 
-assertArraysEqual(flattenRecursive([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+module.exports = flattenRecursive;
