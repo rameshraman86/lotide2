ffb5011a408b8388dbcd79085f1315a37d4e70b4
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-04 06:53:52 -0400

    refactor using index

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,8 @@
-const tail = require('../tail');
 const assert = require('chai').assert;
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-
+const _ = require('../index');
 
 describe('#tail ', () => {
   it('Returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
-    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+    assert.deepEqual(_.tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
 });
