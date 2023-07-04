3b8ab6dcfa2a730ac1bdfe838e048b8b89b96b27
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-03 20:09:36 -0400

    refactored tail.js using exports

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😀Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🥵Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
   if (Array.isArray(arr)) {
